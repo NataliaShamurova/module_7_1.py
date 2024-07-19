63ff33231466df68216ccb4efdcedc4e53cdd2f1
NataliaShamurova <shamurovann@yandex.ru>
2024-07-19 20:45:57 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -13,18 +13,18 @@ class Product:
 
 class Shop():
 
-
     def __init__(self):
         self.__file_name = '7_1.txt'
 
+    
     def get_products(self):
-        file_content = ' '
+
         file = open(self.__file_name, 'r')
         file_contents = file.read()
-
+        #file.close()
         return file_contents
-
         file.close()
+
 
     def add(self, *products):
         for i in products:
@@ -42,12 +42,17 @@ s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
+p4 = Product('wer', 5.5, 'Vegetables')
+p5 = Product('fvc', 5.5, 'Vegetables')
 
 #print(p1)
 print(p2)  # __str__
 #print(p3)
 
 
-s1.add(p1, p2, p3)
+s1.add(p1, p2, p3, p4, p5)
 
 print(s1.get_products())
+f = open('7_1.txt', 'r')
+print('1', f.read())
+print('2', f.read())
